4d59a3f90aca6157117258691930583ba666389d
dsh <445795837@qq.com>
2017-05-15 17:58:10 +0800

    构建了首页Fragment，以及底部导航栏

--- app/src/main/java/com/lottery/activity/HomeActivity.java
+++ app/src/main/java/com/lottery/activity/HomeActivity.java
@@ -1,47 +1,60 @@
 package com.lottery.activity;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
 
+import com.jpeng.jptabbar.JPTabBar;
+import com.jpeng.jptabbar.OnTabSelectListener;
 import com.lottery.R;
+import com.lottery.fragment.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private JPTabBar mTabbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mTabbar = (JPTabBar) findViewById(R.id.tabbar);
+        mTabbar.setTitles(R.string.home, R.string.together_buy, R.string.recharge, R.string.myself)
+                .setNormalIcons(R.mipmap.ic_home, R.mipmap.ic_together_buy, R.mipmap.ic_recharge, R.mipmap.ic_myself)
+                .setSelectedIcons(R.mipmap.ic_home_selected, R.mipmap.ic_together_buy_selected, R.mipmap
+                        .ic_recharge_selected, R.mipmap.ic_myself_selected)
+                .generate();
+        mTabbar.setSelectedColor(ContextCompat.getColor(this, R.color.red));
+        replaceFragment(new HomeFragment());
+        mTabbar.setTabListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int index) {
+                switch (index) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        });
+    }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    /**
+     * 切换fragment页面
+     *
+     * @param fragment
+     */
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction beginTransaction = fragmentManager.beginTransaction();
+        beginTransaction.replace(R.id.content, fragment);
+        beginTransaction.commit();
     }
 
 }
--- app/src/main/java/com/lottery/fragment/HomeFragment.java
+++ app/src/main/java/com/lottery/fragment/HomeFragment.java
@@ -1,30 +1,80 @@
 package com.lottery.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
+import com.lottery.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/5/15 0015.
  */
 
 public class HomeFragment extends BaseFragment {
+    @BindView(R.id.rlv_home)
+    RecyclerView rlv_home;
+    @BindView(R.id.cb_home_top)
+    ConvenientBanner cb_home_top;
+
+    private List<Integer> imageList;
     @Override
     protected View initContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
             savedInstanceState) {
-//        view = inflater.
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     protected void initView(View view) {
-
+        getImageList();
+        cb_home_top.setPages(new CBViewHolderCreator<LocalImageHolderView>() {
+            @Override
+            public LocalImageHolderView createHolder() {
+                return new LocalImageHolderView();
+            }
+        },imageList);
     }
 
     @Override
     protected void getBundleExtras(Bundle bundle) {
 
+    }
+
+    public void getImageList(){
+        imageList = new ArrayList<>();
+        imageList.add(R.mipmap.im1);
+        imageList.add(R.mipmap.im2);
+        imageList.add(R.mipmap.im3);
+        imageList.add(R.mipmap.im4);
+    }
+
+    public class LocalImageHolderView implements Holder<Integer> {
+        private ImageView imageView;
+        @Override
+        public View createView(Context context) {
+            imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
+        }
+
+        @Override
+        public void UpdateUI(Context context, final int position, Integer data) {
+            imageView.setImageResource(data);
+        }
     }
 }
